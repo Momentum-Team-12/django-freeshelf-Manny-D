ec834dad831f3096bb663fff20ecb3d1b7491820
Manny Dador <EADador@gmail.com>
2022-05-10 09:31:15 -0400

    images

--- books/forms.py
+++ books/forms.py
@@ -9,6 +9,8 @@ class BookForm(forms.ModelForm):
             'title',
             'author',
             'description',
+            # 'image',
+            'url',
         ]
 
 class NoteForm(forms.ModelForm):
