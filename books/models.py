6a231e6f57908b555c97818ed1685d1f425db128
Manny Dador <EADador@gmail.com>
2022-05-10 11:35:31 -0400

    beg of Cat n Fav added

--- books/models.py
+++ books/models.py
@@ -1,5 +1,9 @@
+from turtle import title
+from unicodedata import category
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.template.defaultfilters import slugify
+from django.urls import reverse
 
 
 class User(AbstractUser):
@@ -16,6 +20,8 @@ class Book(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(upload_to = 'static/img/')
     url = models.URLField(max_length=200, null=True, blank=True)
+    # one to many relationship - book has one category but category has many books
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, null=True, blank=True, related_name='books')
 
 class Note(models.Model):
     album = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="notes")
@@ -28,3 +34,21 @@ class Image(models.Model):
 
     def __str__(self):
         return self.title
+
+class Category(models.Model):
+    name = models.CharField(max_length=250)
+    slug = models.SlugField(null=False, unique=True)
+
+    def get_absolute_url(self):
+        return reverse('category_detail', kwargs=
+        {'slug': self.slug})
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        return super().save(*args,**kwargs)
+
+class Favorite(models.Model):
+    book = models.ForeignKey('Book', on_delete=models.CASCADE,related_name='favorites')
+    user = models.ForeignKey('User', on_delete=models.CASCADE,related_name ='favorites')
+    
